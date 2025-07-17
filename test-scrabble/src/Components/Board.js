443beafd27ac3c79f9dfce2497641a51e0be9427
jayden-creat3 <jaydenkamau508@gmail.com>
2025-07-17 23:31:09 +0300

    game board

---
+++ test-scrabble/src/Components/Board.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ScrabbleBoard from "./ScrabbleBoard";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Transformers x Gundam Scrabble</h1>
+      <ScrabbleBoard />
+    </div>
+  );
+}
+
+export default App;
--- test-scrabble/src/main.jsx
+++ test-scrabble/src/main.jsx
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Player from "./Components/Player.jsx"
-
+import ScrabbleBoard from "./Components/ScrabbleBoard.jsx"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  
+  <ScrabbleBoard/>
     <Player />
   </StrictMode>,
 )
