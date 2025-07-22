e1e13a67cb7461edd9290adff3067dde643740ef
kamaukimani <paulkimys285@gmail.com>
2025-07-22 11:06:29 +0300

    add file and its contents

--- test-scrabble/src/main.jsx
+++ test-scrabble/src/main.jsx
@@ -2,11 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Player from "./Components/Player.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   
-    <Player />
+    <App />
   </StrictMode>,
 )
